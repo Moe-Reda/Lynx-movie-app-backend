4c6ce042ab6c7034c280e79788b844fbf05239d4
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2023-08-01 17:10:26 -0400

    Initial commit

---
+++ src/main/java/com/mahboubreda/movies/ReviewService.java
@@ -0,0 +1,26 @@
+package com.mahboubreda.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate template;
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+        template.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
